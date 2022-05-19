b5b9d31679de0c963d5af5568783e29022b9aca0
anjanak05 <anjna4523@gmail.com>
2022-05-19 01:54:43 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import {Kite, SubHeadings, MobileManufact, MobileManufactcompanies} from './components/kite';
+
+function App() {
+ 
+  return (
+    <div className="App">
+       <Kite></Kite>
+       <SubHeadings></SubHeadings>
+       <MobileManufact></MobileManufact>
+       <MobileManufactcompanies></MobileManufactcompanies>
+    
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/kite.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+
+const Kite = () =>{
+    return  <h1>Mobile Operaing System</h1>
+};
+
+
+const SubHeadings = () =>{
+    return  <ul>
+        <li>Android</li>
+        <li>Blackberry</li>
+        <li>iPhone</li>
+        <li>Windows Phone</li>
+    </ul>
+};
+
+
+const MobileManufact = () =>{
+    return  <h1>Mobile Manufacturers</h1>
+};
+
+
+const MobileManufactcompanies = () =>{
+    return  <ul>
+        <li>samsung</li>
+        <li>htc</li>
+        <li>nokiia</li>
+        <li>apple</li>
+    </ul>
+};
+
+export  {Kite,SubHeadings, MobileManufact, MobileManufactcompanies };
